ad629f84bae0ffdad1bdf53143a84e55c8f7ba5e
kumarabhilash <abhilashkumar97@gmail.com>
2015-12-30 16:59:44 +0530

    ejbfhgsfssdhsda

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -50,10 +50,10 @@
 # The server-based syntax can be used to override options:
 # ------------------------------------
 server '54.200.232.18',
-  user: 'deploy',
+  user: 'ubuntu',
   roles: %w{web app db},
   ssh_options: {
-    user: 'deploy', # overrides user setting above
+    user: 'ubuntu', # overrides user setting above
     keys: %w(/home/abhilash/test.pem),
     forward_agent: false,
     auth_methods: %w(publickey password)
