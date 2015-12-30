3ae904b5a2de10029f8fe65a9e63d2be373aad1d
kumarabhilash <abhilashkumar97@gmail.com>
2015-12-30 18:23:02 +0530

    ejbfhdfslk

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
-set :default_env, { path: "$PATH" }
+set :default_env, { path: "/home/ubuntu/.rvm/gems/ruby-2.2.1/bin:$PATH" }
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
