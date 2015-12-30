84fc4d203c7d1cd0cf6ec71512ce10709d9a1f16
kumarabhilash <abhilashkumar97@gmail.com>
2015-12-30 16:48:23 +0530

    ejbfhgsfh

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,13 @@
 # config valid only for current version of Capistrano
 
 set :application, 'myapp'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :repo_url, 'git@github.com:abhilashkkumar/myapp.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/var/www/myapp'
 
 # Default value for :scm is :git
 # set :scm, :git
